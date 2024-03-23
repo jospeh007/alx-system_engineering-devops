dd1cf75a011654875512d09f778662e38b316a9b
jospeh007 <youssefgf123@gmail.com>
2024-03-22 17:16:18 -0700

    Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #!/usr/bin/puppet
 # Install a specific version of flask (2.1.0)
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
