afb3b6da4b76dec9573ec213cb02474bb925e47d
jospeh007 <youssefgf123@gmail.com>
2024-04-22 07:39:44 -0700

    Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Using what you did in the task #0, extend your Python script to 
+"""Using what you did in the task #0, extend your Python script to
 export data in the CSV format."""
 
 if __name__ == "__main__":
